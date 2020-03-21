60c4520dc334f94bbc5152bfb207078e1db35ef0
Dharlok <you@example.com>
2020-03-21 23:12:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace thursday_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Type a number:");
+                int n = int.Parse(Console.ReadLine());
+                double chislo = 1.1;
+                double chislo2 = 1.2;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        chislo -= chislo2; 
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        chislo += chislo2;
+                    }
+                    chislo2 += 0.1;
+                }
+                Console.WriteLine("Answer= " + chislo);
+                Console.ReadKey();
+            }
+        }
+    }
+}  
+    
+
